7f1e5556878b6459048899ef7c23f92848e05a89
Blagodatskih Serge <stikkas17@gmail.com>
2025-02-16 12:42:33 +0300

    Main Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -42,6 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.tbuonomo.dotsindicator)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
